7ff3209d49adad0fc85809237139407a7709fdeb
[] <awh938@gmail.com>
2018-02-10 21:05:23 -0500

    bypass name field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
   end
-
+  
   private
 
    def require_sign_in
