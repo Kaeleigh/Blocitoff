21090e9bbd467718a279fb1e8f3c282f8d02dd7b
[] <awh938@gmail.com>
2018-02-19 17:51:39 -0500

    alternative ajax method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,17 +14,16 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
+    item = Item.find(params[:id])
 
-    if @item.delete
+    if item.delete
       flash[:notice] = "Item was completed."
     else
       flash[:error] = "There was an error deleting the item, please try again."
     end
 
     respond_to do |format|
-      format.html
-      format.js
+      format.js { render js: current_user }
     end
   end
 
