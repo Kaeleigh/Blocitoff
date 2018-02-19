52afadf36fe086a1ff93c004843185e0f9dfe1fa
[] <awh938@gmail.com>
2018-02-19 18:53:54 -0500

    fix alt

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,17 +14,19 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
+    @item = Item.find(params[:id])
 
-    if item.delete
+    if @item.delete
       flash[:notice] = "Item was completed."
     else
       flash[:error] = "There was an error deleting the item, please try again."
     end
 
     respond_to do |format|
-      format.js { render js: current_user }
+      format.html
+      format.js
     end
+    redirect_to root_path
   end
 
   private
